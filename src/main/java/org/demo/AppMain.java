1a7e227f37f13abe56f1d19a1dc6177751e208b2
Felipe <lipe.coutinho13@gmail.com>
2024-11-01 20:11:35 -0300

    FELIPE DEMO NOVO 01_11

--- src/main/java/org/demo/AppMain.java
+++ src/main/java/org/demo/AppMain.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class AppMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        String pathMain="C:\\Users\\Unifan\\IdeaProjects\\demo\\src\\main\\java\\org\\demo\\view\\MainView.fxml";
+        String pathMain="C:\\Users\\Unifan\\Documents\\demo\\src\\main\\java\\org\\demo\\view\\MainView.fxml";
         FXMLLoader fxmlLoader = new FXMLLoader();
         Parent root = fxmlLoader.load(new FileInputStream(pathMain));
         Scene scene = new Scene(root, 800, 550);
