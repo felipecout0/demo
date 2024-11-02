5b15243c873c9a8fe2536c613d9512efc9187f8e
Felipe <lipe.coutinho13@gmail.com>
2024-11-01 21:30:26 -0300

    dev nome 01_11

---
+++ src/main/java/model/demo.java
@@ -0,0 +1,31 @@
+package model;
+
+public class demo {
+    private String nome;
+    private String senha;
+
+    //construtor
+    public demo() {
+    }
+
+    public demo(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
--- src/main/java/org/demo/controller/MainController.java
+++ src/main/java/org/demo/controller/MainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.demo;
 
 public class MainController {
     @FXML
@@ -12,9 +13,16 @@ public class MainController {
     private  TextField txtSenha;
     @FXML
     private Button btnSalvar;
+    private demo demo;
 
+    //**************************q
     @FXML
     public void onBtnSalvarAction(){
-        System.out.println("Clique Salvar");
+        this.demo = new demo();
+        demo.setNome( txtNome.getText() );
+        demo.setSenha( txtSenha.getText() );
+        System.out.println("Nome: " + demo.getNome() );
+        System.out.println(" Senha: " +demo.getSenha() );
+
     }
 }
